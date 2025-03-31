2fc2d38a01e774c6e652a008d5af2739869dbffb
Alexandr Tantsiura <o.tantsiura@spd-ukraine.com>
2025-03-31 12:58:01 +0300

    Fix router

--- src/pages/router.tsx
+++ src/pages/router.tsx
@@ -2,9 +2,17 @@ import { createBrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
+const BASE_PATH = "/luciad-map-demo/";
+
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
   },
-]);
+  {
+    path: "*",
+    element: <div>Not Found</div>,
+  }
+], {
+  basename: BASE_PATH,
+});
